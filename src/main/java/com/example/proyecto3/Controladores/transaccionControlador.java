1e840e2f690a95f92ce1e85a0884f0875b243d18
Cristian Lopez <cristian__host@hotmail.com>
2022-09-24 22:05:46 -0500

    v4

--- src/main/java/com/example/proyecto3/Controladores/transaccionControlador.java
+++ src/main/java/com/example/proyecto3/Controladores/transaccionControlador.java
@@ -1,52 +1,50 @@
 package com.example.proyecto3.Controladores;
-
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.proyecto3.Modelo.Transaccion;
-import com.example.proyecto3.Servicios.TransaccionServicios;
+import com.example.proyecto3.Repositorios.TransaccionRepositorio;
 
 @RestController
-@RequestMapping("/transacciones")
+@RequestMapping("/Transaccion")
 public class TransaccionControlador {
 
     @Autowired
-    private TransaccionServicios servicios;
+    private TransaccionRepositorio repositorioTransaccion;
 
-    public TransaccionControlador(TransaccionServicios servicios){
-        this.servicios = servicios;
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public List<Transaccion> listar() {
+        return repositorioTransaccion.findAll();
 
-    }
-    @PostMapping
-    private Transaccion guardar(@RequestBody Transaccion transaccion){
-        return this.servicios.create(transaccion);
     }
 
-    @GetMapping("/listas_transacciones")
-    public List<Transaccion> listas_transacciones(){
-        return this.servicios.getAllTransacciones();
-    }
-    @PutMapping
-    public Transaccion actualizar(@RequestBody Transaccion transaccion){
-        return this.servicios.update(transaccion);
-    }
-    @DeleteMapping
-    public void eliminar(@RequestBody Transaccion transaccion){
-        this.servicios.delete(transaccion);
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    public Transaccion obtenerId(@PathVariable Long id) {
+        return repositorioTransaccion.findById(id).get();
+
     }
 
-    @GetMapping(value = "/{id}")
-    public Optional<Transaccion>listaPorId(@PathVariable ("id") Long id){
-        return this.servicios.porId(id);
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public Transaccion agregaTransaccion(@RequestBody Transaccion Transaccion) {
+        return repositorioTransaccion.save(Transaccion);
     }
+
+    @RequestMapping(value = "/modify", method = RequestMethod.PATCH)
+    public Transaccion patch(@RequestBody Transaccion Transaccion, @PathVariable Long id) {
+        return repositorioTransaccion.save(Transaccion);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public void eliminarTransaccion(@PathVariable Long id) {
+       repositorioTransaccion.deleteById(id);
+
+    }
+    
+
 }
